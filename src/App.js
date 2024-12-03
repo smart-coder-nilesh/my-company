3090c5199052af3110fa51352aec7e8b70ab826f
Nilesh <nilesh.mishra@hexaviewtech.com>
2024-12-03 22:32:25 +0530

    ADDED ROOT

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to My React App!</h1>
-      <p>This is a simple app deployed on GitHub Pages.</p>
-    </div>
+    document.getElementById('root')
   );
 }
 
